ef0fcf5695b300d32133c68739a288f85dd9f416
Alex Rodrigues <alexsantosro@hotmail.com>
2024-05-31 11:34:06 -0300

    Code improved

--- functions.php
+++ functions.php
@@ -1,5 +1,35 @@
 <?php
 
+function pageBanner($args = NULL) { //Analisa o argumento, se não houver considera como nulo.
+  
+  if (!isset($args['title'])) {
+    $args['title'] = get_the_title();
+  }
+ 
+  if (!isset($args['subtitle'])) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+ 
+  if (!isset($args['photo'])) {
+    if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
+      $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+      $args['photo'] = get_theme_file_uri('/images/ocean.jpg'); //Fallback se não houver imagem de fundo.
+    }
+  }
+
+  ?>
+ <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo'] ?>);"></div> <!-- O campo gerado de imagem do ACF é um Array. Nesse caso pegamos a imagem olhando dentro da propriedade sizes e pegamos o tamanho criado em functions -->
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+        <div class="page-banner__intro">
+          <p><?php echo $args['subtitle'] ?></p>
+        </div>
+      </div>  
+    </div>
+<?php }
+
 function university_files() {
   wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -15,6 +45,7 @@ function university_features() { //Função que ativa recursos do wordpress
   add_theme_support('post-thumbnails'); //Ativa as thumbnails nos blog posts.
   add_image_size('professorLandscape', 400, 260, true); //Argumentos - a: Nick name image size, b: wide size, c: tall size, d: Crop or not
   add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
 
 }
 
--- single-program.php
+++ single-program.php
@@ -3,17 +3,8 @@
 get_header();
 
 while (have_posts()) {
-  the_post(); ?>
-  <div class="page-banner">
-    <div class="page-banner__bg-image"
-      style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>DONT FORGET TO REPLACE ME LATER</p>
-      </div>
-    </div>
-  </div>
+  the_post(); 
+  pageBanner();?>
 
   <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
@@ -51,7 +42,7 @@ while (have_posts()) {
         $RelatedProfessors->the_post(); ?> 
         <li class="professor-card__list-item">
           <a class="professor-card" href="<?php the_permalink(); ?>">
-        <img class="professor-card__image" src="<?php the_post_thumbnail_url(); ?>">
+        <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
         <span class="professor_card__name"><?php the_title(); ?></span>
         </a>
       </li>
@@ -91,28 +82,10 @@ while (have_posts()) {
       echo '<h2 class="headline head-line--medium">Upcoming ' . get_the_title() . ' Event</h2>';
 
       while ($homepageEvents->have_posts()) {
-        $homepageEvents->the_post(); ?>
-        <div class="event-summary">
-          <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month"><?php
-            $eventDate = new DateTime(get_field('event_date'));
-            echo $eventDate->format('M')
-              ?></span>
-            <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a
-                href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p><?php if (has_excerpt()) {
-              echo get_the_excerpt();
-            } else {
-              echo wp_trim_words(get_the_content(), 18);
-            } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-          </div>
-        </div>
-      <?php }
+        $homepageEvents->the_post();
+        get_template_part('template-parts/event');//Função que puxa uma parte repetida do template, recebe dois argumentos. O primeiro é o caminho onde está o conteúdo.
+       }
     }
-
     ?>
   </div>
 
